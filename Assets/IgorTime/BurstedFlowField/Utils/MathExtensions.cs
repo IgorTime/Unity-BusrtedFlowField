e3c254c751ac0e4d2b1f492e0fdb80c1ef5accec
igor.semenov <igor.semenov.minsk@gmail.com>
2023-07-03 19:00:33 +0200

    Math extension

--- Assets/IgorTime/BurstedFlowField/Utils/MathExtensions.cs
+++ Assets/IgorTime/BurstedFlowField/Utils/MathExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Unity.Mathematics;
+﻿using System.Runtime.CompilerServices;
+using Unity.Burst;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace IgorTime.BurstedFlowField
@@ -10,5 +12,9 @@ namespace IgorTime.BurstedFlowField
         public static Vector3 X0Y_Vector3(this in int2 int2) => new(int2.x, 0, int2.y);
 
         public static float3 X0Y_Float3(this in float2 float2) => new(float2.x, 0, float2.y);
+        
+        [BurstCompile]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Squared(this in float value) => value * value;
     }
 }
